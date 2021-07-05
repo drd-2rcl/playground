6312ca09d775b863b7b6d89158ebf8beab8104e8
drd-2rcl <drd.rclrd@gmail.com>
2021-07-05 20:46:47 -0300

    Setup PubSub broadcast

--- live_view_todos/lib/live_view_todos/todos.ex
+++ live_view_todos/lib/live_view_todos/todos.ex
@@ -8,6 +8,16 @@ defmodule LiveViewTodos.Todos do
 
   alias LiveViewTodos.Todos.Todo
 
+  @topic inspect(__MODULE__)
+
+  def subscribe do
+    Phoenix.PubSub.subscribe(LiveViewTodos.PubSub, @topic)
+  end
+
+  defp broadcast_change({:ok, result}, event) do
+    Phoenix.PubSub.broadcast(LiveViewTodos.PubSub, @topic, {__MODULE__, event, result})
+  end
+
   @doc """
   Returns the list of todos.
 
@@ -53,6 +63,7 @@ defmodule LiveViewTodos.Todos do
     %Todo{}
     |> Todo.changeset(attrs)
     |> Repo.insert()
+    |> broadcast_change([:todo, :created])
   end
 
   @doc """
@@ -71,6 +82,7 @@ defmodule LiveViewTodos.Todos do
     todo
     |> Todo.changeset(attrs)
     |> Repo.update()
+    |> broadcast_change([:todo, :updated])
   end
 
   @doc """
@@ -86,7 +98,9 @@ defmodule LiveViewTodos.Todos do
 
   """
   def delete_todo(%Todo{} = todo) do
-    Repo.delete(todo)
+    todo
+    |> Repo.delete(todo)
+    |> broadcast_change([:todo, :deleted])
   end
 
   @doc """
